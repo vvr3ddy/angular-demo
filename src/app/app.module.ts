60df300d9a773c82b71747510bd6eabe26ffac74
vvr3ddy <vvr-rockstar@bmsit.in>
2021-05-10 18:26:41 +0530

    :octocat: :sparkles: Create EmployeeCRUD Project
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeDetailsComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
